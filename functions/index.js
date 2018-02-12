3aee9aa0f1d337f92bbc088c04e69b245670bf4d
Jacob Nogle <jnogle@calpoly.edu>
2018-02-11 18:00:38 -0800

    added requested changes

--- functions/index.js
+++ functions/index.js
@@ -13,15 +13,15 @@ const db = admin.firestore();
 * Expects email, password, first/last name, phone number, and photo url *
 *  in request body                                                      *
 *************************************************************************/
-app.post('/users/register-user',(req, res) => {
-	var password = req.body.password;
-	var email = req.body.email;
-	var first = req.body.first;
-	var last = req.body.last;
-	var phone = req.body.phone;
+app.post('/users/register-user', (req, res) => {
+    var password = req.body.password;
+    var email = req.body.email;
+    var first = req.body.first;
+    var last = req.body.last;
+    var phone = req.body.phone;
     var imageUrl = req.body.imageLink;  // can get this after uploading an image to google cloud storage from client-side
 
-	if(typeof phone === 'undefined') {
+    if (typeof phone === 'undefined') {
         var json_res = {
             "message": "Validation failed",
             "name": "ValidationError",
@@ -36,8 +36,8 @@ app.post('/users/register-user',(req, res) => {
         }
         return res.status(400).json(json_res);
     }
-    if(isNaN(phone) || phone.length !== 10) {
-    	var json_res = {
+    if (isNaN(phone) || phone.length !== 10) {
+        var json_res = {
             "message": "Validation failed",
             "name": "ValidationError",
             "errors": {
@@ -51,7 +51,7 @@ app.post('/users/register-user',(req, res) => {
         }
         return res.status(400).json(json_res);
     }
-	if(typeof password === 'undefined' || password.length === 0) {
+    if (typeof password === 'undefined' || password.length === 0) {
         var json_res = {
             "message": "Validation failed",
             "name": "ValidationError",
@@ -64,9 +64,24 @@ app.post('/users/register-user',(req, res) => {
                 }
             }
         }
-            return res.status(400).json(json_res);
+        return res.status(400).json(json_res);
     }
-    if(typeof email === 'undefined' || email.length === 0) {
+    if (password.length < 8 || password.length > 72) {
+        var json_res = {
+            "message": "Validation failed",
+            "name": "ValidationError",
+            "errors": {
+                "name": {
+                    "message": "Passwords must be between 8 and 72 characters",
+                    "name": "ValidatorError",
+                    "path": "password",
+                    "type": "length"
+                }
+            }
+        }
+        return res.status(400).json(json_res);
+    }
+    if (typeof email === 'undefined' || email.length === 0) {
         var json_res = {
             "message": "Validation failed",
             "name": "ValidationError",
@@ -79,10 +94,10 @@ app.post('/users/register-user',(req, res) => {
                 }
             }
         }
-            return res.status(400).json(json_res);
+        return res.status(400).json(json_res);
     }
-    if(typeof first === 'undefined' || typeof last === 'undefined'
-    	|| first.length === 0 || last.length === 0) {
+    if (typeof first === 'undefined' || typeof last === 'undefined'
+        || first.length === 0 || last.length === 0) {
         var json_res = {
             "message": "Validation failed",
             "name": "ValidationError",
@@ -95,41 +110,26 @@ app.post('/users/register-user',(req, res) => {
                 }
             }
         }
-            return res.status(400).json(json_res);
-    }
-    if(password.length < 8 || password.length > 72) {
-    	var json_res = {
-            "message": "Validation failed",
-            "name": "ValidationError",
-            "errors": {
-                "name": {
-                    "message": "Passwords must be between 8 and 72 characters",
-                    "name": "ValidatorError",
-                    "path": "password",
-                    "type": "length"
-                }
-            }
-        }
-            return res.status(400).json(json_res);
+        return res.status(400).json(json_res);
     }
     const newUser = {
-  		email: email,
-  		emailVerified: false,   // User verification email must be sent from client-side for this to become true (no server-side function for this currently available)
-  		phoneNumber: "+1" + phone, 
-  		password: password,
-  		displayName: first + " " + last,
-  		photoURL: imageUrl
-	};
-  	admin.auth().createUser(newUser)
-  	 .then(function(userRecord) {
-    	// See the UserRecord reference doc for the contents of userRecord.
-    	console.log("Successfully created new user:", userRecord.uid);
-    	return res.status(200).json(newUser);
- 	 })
-  	 .catch(function(error) {
-    	console.log("Error creating new user:", error);
-    	return res.status(400).send(error);
-  	 });
+        email: email,
+        emailVerified: false,   // User verification email must be sent from client-side for this to become true (no server-side function for this currently available)
+        phoneNumber: "+1" + phone,
+        password: password,
+        displayName: first + " " + last,
+        photoURL: imageUrl
+    };
+    admin.auth().createUser(newUser)
+        .then(function (userRecord) {
+            // See the UserRecord reference doc for the contents of userRecord.
+            console.log("Successfully created new user:", userRecord.uid);
+            return res.status(200).json(newUser);
+        })
+        .catch(function (error) {
+            console.log("Error creating new user:", error);
+            return res.status(400).send(error);
+        });
 });
 
 /************************************************************************
@@ -140,11 +140,11 @@ app.post('/users/register-user',(req, res) => {
 app.delete('/users/delete-user/:uid', (req, res) => {
     const uid = req.params.uid;
     admin.auth().deleteUser(uid)
-        .then(function() {
+        .then(function () {
             console.log("Successfully deleted user");
             return res.status(200).send();
         })
-        .catch(function(error) {
+        .catch(function (error) {
             console.log("Error deleting user:", error);
             return res.status(400).send(error);
         })
@@ -158,19 +158,20 @@ exports.api = functions.https.onRequest(app);
 * Triggered by new user creation                                   *
 ********************************************************************/
 exports.storeNewUser = functions.auth.user().onCreate(event => {
-	const uid = event.data.uid;
-	const email = event.data.email;
-	const phoneNumber = event.data.phoneNumber;
-	const name = event.data.displayName;
+    const uid = event.data.uid;
+    const email = event.data.email;
+    const phoneNumber = event.data.phoneNumber;
+    const name = event.data.displayName;
     const photoURL = event.data.photoURL;
-    
-	const coll = db.collection("users");
+
+    const coll = db.collection("users");
     //Add default data 
-	return coll.doc(uid).set({
-		email : email,
-		phoneNumber : phoneNumber,
-		name: name,
+    return coll.doc(uid).set({
+        email: email,
+        phoneNumber: phoneNumber,
+        name: name,
         profile: {
+            imageLink: photoURL,
             isPublic: true,
             isStaff: false,
             isCommunityGroupLeader: false,
@@ -183,17 +184,17 @@ exports.storeNewUser = functions.auth.user().onCreate(event => {
             summerMissionUpdates: true
         },
         permissions: {
-            isAdmin: false, 
-            isVerified: false 
+            isAdmin: false,    //CHANGE THIS TO ADD NEW DOC IN PERMISSIONS COLLECTION
+            isVerified: false
         },
         lastActive: Date.now()
-	})
-	.then(function() {
-		console.log("New user successfully added to database");
-	})
-	.catch(function(error) {
-		console.error("Error adding new user to database: ", error);
-	});
+    })
+        .then(function () {
+            console.log("New user successfully added to database");
+        })
+        .catch(function (error) {
+            console.error("Error adding new user to database: ", error);
+        });
 });
 
 /*******************************************************************
@@ -206,10 +207,10 @@ exports.removeUser = functions.auth.user().onDelete(event => {
     const coll = db.collection("users");
 
     return coll.doc(uid).delete()
-    .then(function() {
-        console.log("User successfully deleted from database");
-    })
-    .catch(function(error) {
-        console.error("Error deleting user from database")
-    })
+        .then(function () {
+            console.log("User successfully deleted from database");
+        })
+        .catch(function (error) {
+            console.error("Error deleting user from database")
+        })
 });
